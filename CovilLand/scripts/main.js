ed0375f1c5a5eb6c56a8262323ebbc06a8a65385
nicolorenzo <nicolorenzo@live.com>
2022-06-28 18:28:12 -0300

    ajustando tempo

--- CovilLand/scripts/main.js
+++ CovilLand/scripts/main.js
@@ -3,61 +3,55 @@ const jean = document.querySelector('.jean');
 const pikachu = document.querySelector('.pikachu');
 
 const pulo = () => {
-    
-    paulo.classList.add('pulo');
+  paulo.classList.add('pulo');
 
-    setTimeout(() => {
-        
-        paulo.classList.remove('pulo');
-
-    }, 600);
-}
+  setTimeout(() => {
+    paulo.classList.remove('pulo');
+  }, 1500);
+};
 
 const loop = setInterval(() => {
+  const jeanPosition = jean.offsetLeft;
+  const pikachuPosition = pikachu.offsetLeft;
+  const pauloPosition = +window
+    .getComputedStyle(paulo)
+    .bottom.replace('px', '');
 
-    const jeanPosition = jean.offsetLeft;
-    const pikachuPosition = pikachu.offsetLeft;
-    const pauloPosition = +window.getComputedStyle(paulo).bottom.replace('px', '');
+  if (jeanPosition <= 120 && jeanPosition > 0 && pauloPosition < 80) {
+    jean.getElementsByClassName.animation = 'none';
+    jean.getElementsByClassName.left = `${jeanPosition}px`;
 
-    if(jeanPosition <= 120 && jeanPosition > 0 && pauloPosition < 80) {
+    paulo.getElementsByClassName.animation = 'none';
+    paulo.getElementsByClassName.bottom = `${pauloPosition}px`;
 
-        jean.getElementsByClassName.animation = 'none';
-        jean.getElementsByClassName.left = `${jeanPosition}px`;
+    paulo.src = './imagens/blockdorei.png';
+    paulo.style.width = '175px';
+    paulo.style.marginLeft = '150px';
 
-        paulo.getElementsByClassName.animation = 'none';
-        paulo.getElementsByClassName.bottom = `${pauloPosition}px`;
-
-        paulo.src ='./imagens/blockdorei.png';
-        paulo.style.width = '175px'
-        paulo.style.marginLeft = '150px'
-
-        clearInterval(loop);
-    }   
-
-}, 10)
+    clearInterval(loop);
+  }
+}, 10);
 
 const loop1 = setInterval(() => {
+  const jeanPosition = jean.offsetLeft;
+  const pikachuPosition = pikachu.offsetLeft;
+  const pauloPosition = +window
+    .getComputedStyle(paulo)
+    .bottom.replace('px', '');
 
-    const jeanPosition = jean.offsetLeft;
-    const pikachuPosition = pikachu.offsetLeft;
-    const pauloPosition = +window.getComputedStyle(paulo).bottom.replace('px', '');
+  if (pikachuPosition <= 120 && pikachuPosition > 0 && pauloPosition < 90) {
+    pikachu.getElementsByClassName.animation = 'none';
+    pikachu.getElementsByClassName.left = `${pikachuPosition}px`;
 
-    if(pikachuPosition <= 120 && pikachuPosition > 0 && pauloPosition < 90) {
+    paulo.getElementsByClassName.animation = 'none';
+    paulo.getElementsByClassName.bottom = `${pauloPosition}px`;
 
-        pikachu.getElementsByClassName.animation = 'none';
-        pikachu.getElementsByClassName.left = `${pikachuPosition}px`;
+    paulo.src = './imagens/pikachuladrao.png';
+    paulo.style.width = '175px';
+    paulo.style.marginLeft = '150px';
 
-        paulo.getElementsByClassName.animation = 'none';
-        paulo.getElementsByClassName.bottom = `${pauloPosition}px`;
-
-        paulo.src ='./imagens/pikachuladrao.png';
-        paulo.style.width = '175px'
-        paulo.style.marginLeft = '150px'
-
-        clearInterval(loop1);
-    }   
-
-}, 10)
-
+    clearInterval(loop1);
+  }
+}, 10);
 
 document.addEventListener('keydown', pulo);
